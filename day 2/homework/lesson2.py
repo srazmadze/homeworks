4cd18aaf464201a87c6b5187ce9dae33b8f373f0
unknown <srazmadze@gmaiil.com>
2023-10-16 23:05:28 -0700

    second commit

---
+++ day 2/homework/lesson2.py
@@ -0,0 +1,13 @@
+#create variable from ages of your family members 
+#გამოითვვალეთ რამდენჯერ აღემატება თქვენი მამის ასაკი თქვენს ასაკს 10 წლის შემდეგ
+
+#homework ex-1
+age_dad=39
+age_mom=39
+age_sister=20
+age_grandmother=66
+age_uncle=46
+age_my_parrot=2
+
+#homework ex-2
+print(49/25)
