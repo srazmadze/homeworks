5571e05fd6b1a9cc0f7f8440629dd5a22a7aa6de
unknown <srazmadze@gmaiil.com>
2023-10-23 08:03:28 -0700

    fourth commit

---
+++ day 4/classwork/lesson4.py
@@ -0,0 +1,2 @@
+name="sandro"
+print=("name")
---
+++ day 4/homework/lesson4.py
@@ -0,0 +1,12 @@
+mom_age=39 
+dad_age=39 
+sister_age=20
+
+print(mom_age + 20)
+print(dad_age + 20)
+print(sister_age + 20)
+
+user_name= input("what's your name: ")
+print("his name is "+ user_name )
+user_age= input("how old are you: ")
+print(user_name +  " is " + user_age + " years old")
